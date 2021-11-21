e444704d5ef4e75dc7534cb65ffef4dd57b4e202
minha619 <minha619@gmail.com>
2021-11-21 18:00:00 -0500

    update

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
         // Your MySQL username,
         user: 'root',
         // Your MySQL password
-        password: 'Apple798520!',
+        password: 'apple',
         database: 'election'
     },
     console.log('Connected to the election database.')
